17e4ebafded01eef353169deb17e675286f31990
Stefan Vasic <vasicstefan123@gmail.com>
2020-01-26 14:53:08 +0100

    Updates

---
+++ precache-manifest.6c76a904159983836bf4691939d61834.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80aa984433849d0ed121ea14412d7326",
+    "url": "/StefanVasic123/uptime/index.html"
+  },
+  {
+    "revision": "5fd897145093996b745c",
+    "url": "/StefanVasic123/uptime/static/css/main.1b398baf.chunk.css"
+  },
+  {
+    "revision": "d19e15fb03f5e2a1ce96",
+    "url": "/StefanVasic123/uptime/static/js/2.27302516.chunk.js"
+  },
+  {
+    "revision": "5fd897145093996b745c",
+    "url": "/StefanVasic123/uptime/static/js/main.c3fc7b46.chunk.js"
+  },
+  {
+    "revision": "db17d53429f7bc913a64",
+    "url": "/StefanVasic123/uptime/static/js/runtime-main.2298cddd.js"
+  }
+]);
